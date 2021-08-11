76d8444175de4373f99a2a49e59b47a70c6f1284
ViNguyen3747 <vinguyen1172000@gmail.com>
2021-08-10 21:13:40 -0500

    add authorReducer

--- src/redux/reducer/courseReducer.js
+++ src/redux/reducer/courseReducer.js
@@ -4,6 +4,8 @@ export default function coursesReducer(State = [], action) {
   switch (action.type) {
     case types.CREATE_COURSE:
       return [...State, { ...action.course }];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
     default:
       return State;
   }
